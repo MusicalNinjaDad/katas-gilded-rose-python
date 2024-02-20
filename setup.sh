a01561c9ade973db9255915666cb53d3552dab2d
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-20 13:51:11 +0100

    port testcase https://github.com/NotMyself/GildedRose/blob/e026bdde759d34b70611cda19efdbf45dc8e2596/src/GildedRose.Tests/TestAssemblyTests.cs
    breaking from the pure version here by giving an import hint

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv .venv \
 && . .venv/bin/activate \
 && python3 -m ensurepip \
-&& pip install -e .
+&& pip install -e .[test]
---
+++ test/test_assemblytests.py
@@ -0,0 +1,4 @@
+import gildedrose_console.gilded_rose
+
+def test_thetruth():
+    assert True
